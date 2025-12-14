9a229e969ddf4fd56262cc30ff3728e5f12c8fd4
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-14 22:42:04 +0530

    Update CSVReader.java

--- 01_csv_reader/CSVReader.java
+++ 01_csv_reader/CSVReader.java
@@ -1,1 +1,29 @@
 
+---
+
+## **6. Sample Code**
+
+### **CSVReader.java**
+```java
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CSVReader {
+    public static void main(String[] args) {
+        String path = "../05_demo_dataset/sample_data.csv";
+        String line;
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                for (String val : values) {
+                    System.out.print(val + "\t");
+                }
+                System.out.println();
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading CSV file: " + e.getMessage());
+        }
+    }
+}
+
