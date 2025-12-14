0e4aead86d6568b0507122a04cc5c6d8dc6b9edb
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-14 22:42:34 +0530

    Update JSONProcessor.java

--- 02_json_processor/JSONProcessor.java
+++ 02_json_processor/JSONProcessor.java
@@ -1,1 +1,18 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class JSONProcessor {
+    public static void main(String[] args) throws Exception {
+        String path = "../05_demo_dataset/sample_data.json";
+        String content = new String(Files.readAllBytes(Paths.get(path)));
+        JSONArray jsonArray = new JSONArray(content);
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject obj = jsonArray.getJSONObject(i);
+            System.out.println("Name: " + obj.getString("name") + ", Age: " + obj.getInt("age"));
+        }
+    }
+}
 
