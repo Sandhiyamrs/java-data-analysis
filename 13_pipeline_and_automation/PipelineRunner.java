66c3f98aa825c2109c7d56e5eb9c3d3224c34d6e
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-26 22:08:54 +0530

    Create PipelineRunner.java

---
+++ 13_pipeline_and_automation/PipelineRunner.java
@@ -0,0 +1,7 @@
+public class PipelineRunner {
+
+    public static void main(String[] args) {
+        PipelineContext context = new PipelineContext();
+        PipelineLogger.log("Pipeline started");
+    }
+}
