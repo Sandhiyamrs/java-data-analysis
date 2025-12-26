733d193450ac1e832ccfe246ef1040bab99e022f
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-26 22:03:12 +0530

    Create DistanceUtils.java

---
+++ 11_machine_learning_basics/DistanceUtils.java
@@ -0,0 +1,10 @@
+public class DistanceUtils {
+
+    public static double euclidean(double[] a, double[] b) {
+        double sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += Math.pow(a[i] - b[i], 2);
+        }
+        return Math.sqrt(sum);
+    }
+}
