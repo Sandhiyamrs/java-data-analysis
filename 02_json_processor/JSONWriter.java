878ddf1dfddfb9b5ef68a0ab933e31d3914a4fa7
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-14 22:03:08 +0530

    Update JSONWriter.java

--- 02_json_processor/JSONWriter.java
+++ 02_json_processor/JSONWriter.java
@@ -1,12 +1,22 @@
 import java.io.*;
+import java.util.Map;
 
 public class JSONWriter {
 
-    public static void write(String filePath, String json)
+    public static void write(String filePath, Map<String, Object> data)
             throws IOException {
 
-        BufferedWriter bw = new BufferedWriter(new FileWriter(filePath));
-        bw.write(json);
-        bw.close();
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+            bw.write("{\n");
+            int count = 0;
+
+            for (Map.Entry<String, Object> entry : data.entrySet()) {
+                bw.write("  \"" + entry.getKey() + "\": \"" + entry.getValue() + "\"");
+                count++;
+                if (count < data.size()) bw.write(",");
+                bw.newLine();
+            }
+            bw.write("}");
+        }
     }
 }
