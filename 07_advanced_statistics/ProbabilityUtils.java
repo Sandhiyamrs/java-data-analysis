bd03ce110e715ad4b4578fda1767ac96a622a9ec
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-14 22:18:12 +0530

    Update ProbabilityUtils.java

--- 07_advanced_statistics/ProbabilityUtils.java
+++ 07_advanced_statistics/ProbabilityUtils.java
@@ -1,18 +1,8 @@
 public class ProbabilityUtils {
 
-    public static double factorial(int n) {
-        double result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
-        }
-        return result;
-    }
-
-    public static double combination(int n, int r) {
-        return factorial(n) / (factorial(r) * factorial(n - r));
-    }
-
-    public static double binomialProbability(int n, int r, double p) {
-        return combination(n, r) * Math.pow(p, r) * Math.pow(1 - p, n - r);
+    public static double probability(int favorable, int total) {
+        if (total == 0)
+            throw new IllegalArgumentException("Total cannot be zero");
+        return (double) favorable / total;
     }
 }
