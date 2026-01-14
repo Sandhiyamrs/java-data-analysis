594deee5b01342c694252b406762a60c19f11dc6
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-14 22:06:45 +0530

    Update MedianCalculator.java

--- 03_statistics/MedianCalculator.java
+++ 03_statistics/MedianCalculator.java
@@ -3,10 +3,17 @@ import java.util.*;
 public class MedianCalculator {
 
     public static double median(List<Double> data) {
-        Collections.sort(data);
-        int n = data.size();
-        return n % 2 == 0
-                ? (data.get(n/2 - 1) + data.get(n/2)) / 2
-                : data.get(n/2);
+        if (data == null || data.isEmpty())
+            throw new IllegalArgumentException("Dataset is empty");
+
+        List<Double> sorted = new ArrayList<>(data);
+        Collections.sort(sorted);
+
+        int n = sorted.size();
+        if (n % 2 == 0) {
+            return (sorted.get(n / 2 - 1) + sorted.get(n / 2)) / 2.0;
+        }
+        return sorted.get(n / 2);
     }
 }
+
