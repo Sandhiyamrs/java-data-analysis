e374be9a887e4cc111cdb96f775dd45977d2916c
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-14 22:02:49 +0530

    Update JSONFlattener.java

--- 02_json_processor/JSONFlattener.java
+++ 02_json_processor/JSONFlattener.java
@@ -2,11 +2,25 @@ import java.util.*;
 
 public class JSONFlattener {
 
-    public static Map<String, String> flatten(Map<String, Object> json) {
-        Map<String, String> flat = new HashMap<>();
-        for (Map.Entry<String, Object> entry : json.entrySet()) {
-            flat.put(entry.getKey(), entry.getValue().toString());
+    public static Map<String, Object> flatten(
+            Map<String, Object> nestedMap,
+            String parentKey
+    ) {
+        Map<String, Object> flatMap = new HashMap<>();
+
+        for (Map.Entry<String, Object> entry : nestedMap.entrySet()) {
+            String newKey = parentKey.isEmpty()
+                    ? entry.getKey()
+                    : parentKey + "." + entry.getKey();
+
+            Object value = entry.getValue();
+
+            if (value instanceof Map) {
+                flatMap.putAll(flatten((Map<String, Object>) value, newKey));
+            } else {
+                flatMap.put(newKey, value);
+            }
         }
-        return flat;
+        return flatMap;
     }
 }
