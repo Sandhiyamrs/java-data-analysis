770041f19a3fecf49e845f15057ad8799d8c4ed5
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-14 22:16:45 +0530

    Update CorrelationAnalysis.java

--- 07_advanced_statistics/CorrelationAnalysis.java
+++ 07_advanced_statistics/CorrelationAnalysis.java
@@ -2,32 +2,21 @@ import java.util.*;
 
 public class CorrelationAnalysis {
 
-    public static double pearsonCorrelation(List<Double> x, List<Double> y) {
-        if (x.size() != y.size()) {
-            throw new IllegalArgumentException("Lists must be of same length");
-        }
+    public static double pearson(List<Double> x, List<Double> y) {
+        if (x.size() != y.size())
+            throw new IllegalArgumentException("Datasets must be same size");
 
-        double meanX = mean(x);
-        double meanY = mean(y);
+        double meanX = x.stream().mapToDouble(Double::doubleValue).average().orElse(0);
+        double meanY = y.stream().mapToDouble(Double::doubleValue).average().orElse(0);
 
-        double numerator = 0;
-        double denomX = 0;
-        double denomY = 0;
+        double num = 0, denX = 0, denY = 0;
 
         for (int i = 0; i < x.size(); i++) {
-            double dx = x.get(i) - meanX;
-            double dy = y.get(i) - meanY;
-            numerator += dx * dy;
-            denomX += dx * dx;
-            denomY += dy * dy;
+            num += (x.get(i) - meanX) * (y.get(i) - meanY);
+            denX += Math.pow(x.get(i) - meanX, 2);
+            denY += Math.pow(y.get(i) - meanY, 2);
         }
 
-        return numerator / Math.sqrt(denomX * denomY);
-    }
-
-    private static double mean(List<Double> data) {
-        double sum = 0;
-        for (double d : data) sum += d;
-        return sum / data.size();
+        return num / Math.sqrt(denX * denY);
     }
 }
