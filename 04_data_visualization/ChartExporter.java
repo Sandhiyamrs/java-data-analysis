684bd59616abbfbd6634ebbbadbeec6604149fd4
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-14 22:08:49 +0530

    Update ChartExporter.java

--- 04_data_visualization/ChartExporter.java
+++ 04_data_visualization/ChartExporter.java
@@ -1,12 +1,16 @@
 import java.io.*;
+import java.util.List;
 
 public class ChartExporter {
 
-    public static void export(String filePath, String chart)
+    public static void export(String filePath, List<Double> data)
             throws IOException {
 
-        BufferedWriter bw = new BufferedWriter(new FileWriter(filePath));
-        bw.write(chart);
-        bw.close();
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+            for (double v : data) {
+                bw.write(String.valueOf(v));
+                bw.newLine();
+            }
+        }
     }
 }
