88b9a2be9dc167bac2ed526f0a9e07f85029bffb
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-14 22:08:00 +0530

    Update SimpleChart.java

--- 04_data_visualization/SimpleChart.java
+++ 04_data_visualization/SimpleChart.java
@@ -1,25 +1,12 @@
-import java.util.*;
-import javax.swing.*;
-import java.awt.*;
+import java.util.List;
 
-public class SimpleChart extends JPanel {
-    private final int[] data = {10, 20, 30, 40, 50};
+public abstract class SimpleChart {
 
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        int width = getWidth() / data.length;
-        for (int i = 0; i < data.length; i++) {
-            int height = data[i] * 5;
-            g.fillRect(i * width + 10, getHeight() - height - 30, width - 20, height);
-        }
+    protected List<Double> data;
+
+    public SimpleChart(List<Double> data) {
+        this.data = data;
     }
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Simple Bar Chart");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
-        frame.add(new SimpleChart());
-        frame.setVisible(true);
-    }
+    public abstract void render();
 }
-
