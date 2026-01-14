6cc0f4e44714b19654f8e9ab921b25085bbdc470
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-14 22:14:30 +0530

    Update DataCleaner.java

--- 06_data_cleaning/DataCleaner.java
+++ 06_data_cleaning/DataCleaner.java
@@ -1,22 +1,11 @@
-import java.util.*;
+import java.util.List;
 
 public class DataCleaner {
 
     public static List<Double> clean(List<Double> data) {
-        List<Double> cleaned = new ArrayList<>(data);
-
-        cleaned = MissingValueHandler.removeMissing(cleaned);
-        cleaned = OutlierDetector.removeOutliers(cleaned);
+        List<Double> cleaned = MissingValueHandler.removeNulls(data);
         cleaned = DuplicateRemover.removeDuplicates(cleaned);
-        cleaned = DataNormalizer.minMaxNormalize(cleaned);
-
-        return cleaned;
-    }
-
-    public static void main(String[] args) {
-        List<Double> rawData = Arrays.asList(10.0, null, 15.0, 1000.0, 15.0, 20.0);
-
-        List<Double> cleanedData = clean(rawData);
-        System.out.println("Cleaned Data: " + cleanedData);
+        cleaned = OutlierDetector.removeOutliers(cleaned);
+        return DataNormalizer.normalize(cleaned);
     }
 }
