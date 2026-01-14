965c9653232c168ef7bce2ad37cbdab0da27225e
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-14 22:17:49 +0530

    Update DistributionAnalyzer.java

--- 07_advanced_statistics/DistributionAnalyzer.java
+++ 07_advanced_statistics/DistributionAnalyzer.java
@@ -2,29 +2,11 @@ import java.util.*;
 
 public class DistributionAnalyzer {
 
-    public static double normalPDF(double x, double mean, double stdDev) {
-        double exponent = -Math.pow(x - mean, 2) / (2 * Math.pow(stdDev, 2));
-        return (1 / (stdDev * Math.sqrt(2 * Math.PI))) * Math.exp(exponent);
-    }
-
-    public static double calculateSkewness(List<Double> data) {
-        double mean = mean(data);
-        double std = stdDev(data, mean);
-        double sum = 0;
-
-        for (double d : data) {
-            sum += Math.pow((d - mean) / std, 3);
-        }
-        return sum / data.size();
-    }
-
-    private static double mean(List<Double> data) {
-        return data.stream().mapToDouble(Double::doubleValue).average().orElse(0);
-    }
-
-    private static double stdDev(List<Double> data, double mean) {
-        double sum = 0;
-        for (double d : data) sum += Math.pow(d - mean, 2);
-        return Math.sqrt(sum / data.size());
+    public static Map<String, Double> summarize(List<Double> data) {
+        Map<String, Double> stats = new HashMap<>();
+        stats.put("min", Collections.min(data));
+        stats.put("max", Collections.max(data));
+        stats.put("mean", data.stream().mapToDouble(Double::doubleValue).average().orElse(0));
+        return stats;
     }
 }
