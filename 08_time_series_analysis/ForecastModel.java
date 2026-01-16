27bbb4bc47936a90e452ba13b6202ea23d0882c6
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-16 22:02:08 +0530

    Update ForecastModel.java

--- 08_time_series_analysis/ForecastModel.java
+++ 08_time_series_analysis/ForecastModel.java
@@ -2,17 +2,7 @@ import java.util.*;
 
 public class ForecastModel {
 
-    public static double naiveForecast(List<Double> data) {
-        return data.get(data.size() - 1);
-    }
-
-    public static List<Double> multiStepForecast(List<Double> data, int steps) {
-        List<Double> forecast = new ArrayList<>();
-        double last = data.get(data.size() - 1);
-
-        for (int i = 0; i < steps; i++) {
-            forecast.add(last);
-        }
-        return forecast;
+    public static double forecastNext(List<Double> data) {
+        return data.stream().mapToDouble(Double::doubleValue).average().orElse(0);
     }
 }
