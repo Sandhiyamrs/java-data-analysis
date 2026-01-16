731b11bdea864f8cc1b637eed3b2789ac5b26d23
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-16 22:54:58 +0530

    Update PipelineContext.java

--- 13_pipeline_and_automation/PipelineContext.java
+++ 13_pipeline_and_automation/PipelineContext.java
@@ -1,7 +1,14 @@
 import java.util.*;
 
 public class PipelineContext {
-    private Map<String, Object> data = new HashMap<>();
-    public void put(String key, Object value) { data.put(key, value); }
-    public Object get(String key) { return data.get(key); }
+
+    private final Map<String, Object> data = new HashMap<>();
+
+    public void put(String key, Object value) {
+        data.put(key, value);
+    }
+
+    public Object get(String key) {
+        return data.get(key);
+    }
 }
