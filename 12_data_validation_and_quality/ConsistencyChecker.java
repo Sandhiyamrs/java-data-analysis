648d10f4fe3c2460f3cc69157ed998156f38cac3
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-16 22:53:46 +0530

    Update ConsistencyChecker.java

--- 12_data_validation_and_quality/ConsistencyChecker.java
+++ 12_data_validation_and_quality/ConsistencyChecker.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class ConsistencyChecker {
 
-    public static boolean consistentLengths(List<?> a, List<?> b) {
+    public static boolean consistent(List<?> a, List<?> b) {
         return a.size() == b.size();
     }
 }
