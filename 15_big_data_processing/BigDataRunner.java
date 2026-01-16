0743c12e379b724a475dab1f99846737ffb07524
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-16 22:59:01 +0530

    Update BigDataRunner.java

--- 15_big_data_processing/BigDataRunner.java
+++ 15_big_data_processing/BigDataRunner.java
@@ -1,17 +1,7 @@
-import java.util.*;
-
 public class BigDataRunner {
 
     public static void main(String[] args) {
-        List<Runnable> tasks = new ArrayList<>();
-
-        for (int i = 1; i <= 5; i++) {
-            int taskId = i;
-            tasks.add(() ->
-                System.out.println("Processing chunk " + taskId)
-            );
-        }
-
-        ParallelProcessor.process(tasks);
+        System.out.println("Big Data Processing Started");
     }
 }
+
