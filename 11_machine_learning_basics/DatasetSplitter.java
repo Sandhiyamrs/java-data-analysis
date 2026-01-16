4cd732b0a180216a908f576fa679ba387428581c
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-16 22:51:41 +0530

    Update DatasetSplitter.java

--- 11_machine_learning_basics/DatasetSplitter.java
+++ 11_machine_learning_basics/DatasetSplitter.java
@@ -2,15 +2,11 @@ import java.util.*;
 
 public class DatasetSplitter {
 
-    public static Map<String, List<double[]>> split(
-            List<double[]> data, double trainRatio) {
-
-        Collections.shuffle(data);
-        int splitIndex = (int) (data.size() * trainRatio);
-
-        Map<String, List<double[]>> result = new HashMap<>();
-        result.put("train", data.subList(0, splitIndex));
-        result.put("test", data.subList(splitIndex, data.size()));
-        return result;
+    public static List<List<double[]>> split(List<double[]> data, double ratio) {
+        int splitIndex = (int) (data.size() * ratio);
+        return List.of(
+                data.subList(0, splitIndex),
+                data.subList(splitIndex, data.size())
+        );
     }
 }
