edea8e714abd43daadec2a3ea7fc44d31be885db
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-16 22:07:22 +0530

    Update SalesDataAnalyzer.java

--- 10_real_world_projects/SalesDataAnalyzer.java
+++ 10_real_world_projects/SalesDataAnalyzer.java
@@ -5,8 +5,4 @@ public class SalesDataAnalyzer {
     public static double totalSales(List<Double> sales) {
         return sales.stream().mapToDouble(Double::doubleValue).sum();
     }
-
-    public static double averageSales(List<Double> sales) {
-        return totalSales(sales) / sales.size();
-    }
 }
