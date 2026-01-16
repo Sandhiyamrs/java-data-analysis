bc3821e21bbdfe40365d3bf8db66b721d8cdf423
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-16 22:49:22 +0530

    Update LogFileAnalyzer.java

--- 10_real_world_projects/LogFileAnalyzer.java
+++ 10_real_world_projects/LogFileAnalyzer.java
@@ -2,13 +2,9 @@ import java.util.*;
 
 public class LogFileAnalyzer {
 
-    public static Map<String, Integer> countLogLevels(List<String> logs) {
-        Map<String, Integer> counts = new HashMap<>();
-
-        for (String log : logs) {
-            String level = log.split(" ")[0];
-            counts.put(level, counts.getOrDefault(level, 0) + 1);
-        }
-        return counts;
+    public static long errorCount(List<String> logs) {
+        return logs.stream()
+                .filter(l -> l.contains("ERROR"))
+                .count();
     }
 }
