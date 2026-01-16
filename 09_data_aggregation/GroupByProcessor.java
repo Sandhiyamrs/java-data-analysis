a7bbfc56c8a6ea23d7198fff4a3a48b6c95686ac
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-16 22:03:14 +0530

    Update GroupByProcessor.java

--- 09_data_aggregation/GroupByProcessor.java
+++ 09_data_aggregation/GroupByProcessor.java
@@ -2,11 +2,10 @@ import java.util.*;
 
 public class GroupByProcessor {
 
-    public static Map<String, List<Double>> groupBy(
+    public static Map<String, List<Double>> group(
             List<String> keys, List<Double> values) {
 
         Map<String, List<Double>> grouped = new HashMap<>();
-
         for (int i = 0; i < keys.size(); i++) {
             grouped
                 .computeIfAbsent(keys.get(i), k -> new ArrayList<>())
