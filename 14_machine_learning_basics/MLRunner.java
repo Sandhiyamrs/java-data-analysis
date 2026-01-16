6d30f7fbab5930058d68526075e07e2f7999bbd7
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-16 22:57:11 +0530

    Update MLRunner.java

--- 14_machine_learning_basics/MLRunner.java
+++ 14_machine_learning_basics/MLRunner.java
@@ -1,19 +1,6 @@
-import java.util.*;
-
 public class MLRunner {
+
     public static void main(String[] args) {
-        List<double[]> X = List.of(
-            new double[]{1, 2},
-            new double[]{2, 3},
-            new double[]{3, 4},
-            new double[]{6, 7}
-        );
-        List<Integer> y = List.of(0, 0, 1, 1);
-
-        KNNClassifier knn = new KNNClassifier(3);
-        knn.fit(X, y);
-
-        int prediction = knn.predict(new double[]{2, 2});
-        System.out.println("Predicted class: " + prediction);
+        System.out.println("ML Pipeline Initialized");
     }
 }
