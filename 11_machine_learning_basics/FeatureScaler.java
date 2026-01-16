4148840b0bf91dff33a117159fa6a83972805fc9
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-16 22:51:17 +0530

    Update FeatureScaler.java

--- 11_machine_learning_basics/FeatureScaler.java
+++ 11_machine_learning_basics/FeatureScaler.java
@@ -1,15 +1,6 @@
 public class FeatureScaler {
 
-    public static double[] minMax(double[] data) {
-        double min = Double.MAX_VALUE, max = Double.MIN_VALUE;
-        for (double d : data) {
-            min = Math.min(min, d);
-            max = Math.max(max, d);
-        }
-        double[] scaled = new double[data.length];
-        for (int i = 0; i < data.length; i++) {
-            scaled[i] = (data[i] - min) / (max - min);
-        }
-        return scaled;
+    public static double scale(double value, double min, double max) {
+        return (value - min) / (max - min);
     }
 }
