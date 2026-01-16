d731800742f274cf35b77544e779e2277780d1c4
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-16 22:58:40 +0530

    Update LargeFileStats.java

--- 15_big_data_processing/LargeFileStats.java
+++ 15_big_data_processing/LargeFileStats.java
@@ -1,12 +1,8 @@
-import java.io.*;
+import java.util.*;
 
 public class LargeFileStats {
 
-    public static int lineCount(String file) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        int count = 0;
-        while (br.readLine() != null) count++;
-        br.close();
-        return count;
+    public static long countLines(List<String> data) {
+        return data.size();
     }
 }
