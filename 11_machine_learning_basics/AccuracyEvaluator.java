d2cb2573ba2ce69ac3ba1908ce9141a4a4618a65
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-16 22:52:23 +0530

    Update AccuracyEvaluator.java

--- 11_machine_learning_basics/AccuracyEvaluator.java
+++ 11_machine_learning_basics/AccuracyEvaluator.java
@@ -5,9 +5,7 @@ public class AccuracyEvaluator {
     public static double accuracy(List<Integer> actual, List<Integer> predicted) {
         int correct = 0;
         for (int i = 0; i < actual.size(); i++) {
-            if (actual.get(i).equals(predicted.get(i))) {
-                correct++;
-            }
+            if (actual.get(i).equals(predicted.get(i))) correct++;
         }
         return (double) correct / actual.size();
     }
