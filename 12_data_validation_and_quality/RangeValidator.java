c01ca18c45c89e1498804095dc8276f15fd38970
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-16 22:53:13 +0530

    Update RangeValidator.java

--- 12_data_validation_and_quality/RangeValidator.java
+++ 12_data_validation_and_quality/RangeValidator.java
@@ -1,6 +1,8 @@
+import java.util.*;
+
 public class RangeValidator {
 
-    public static boolean inRange(double value, double min, double max) {
-        return value >= min && value <= max;
+    public static boolean validate(List<Double> data, double min, double max) {
+        return data.stream().allMatch(v -> v >= min && v <= max);
     }
 }
