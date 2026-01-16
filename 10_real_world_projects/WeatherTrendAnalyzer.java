7990fad2b2b166a64099e5c66bb16dc947088121
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-16 22:50:01 +0530

    Update WeatherTrendAnalyzer.java

--- 10_real_world_projects/WeatherTrendAnalyzer.java
+++ 10_real_world_projects/WeatherTrendAnalyzer.java
@@ -5,8 +5,4 @@ public class WeatherTrendAnalyzer {
     public static double averageTemperature(List<Double> temps) {
         return temps.stream().mapToDouble(Double::doubleValue).average().orElse(0);
     }
-
-    public static String climateTrend(double avgTemp) {
-        return avgTemp > 30 ? "Hot Climate" : "Moderate Climate";
-    }
 }
