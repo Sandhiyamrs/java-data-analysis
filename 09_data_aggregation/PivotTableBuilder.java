358f8eca9e0058785ec065e9ba995409cbdba515
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-16 22:03:44 +0530

    Update PivotTableBuilder.java

--- 09_data_aggregation/PivotTableBuilder.java
+++ 09_data_aggregation/PivotTableBuilder.java
@@ -2,18 +2,13 @@ import java.util.*;
 
 public class PivotTableBuilder {
 
-    public static Map<String, Map<String, Double>> pivot(
-            List<String> rows,
-            List<String> columns,
-            List<Double> values) {
+    public static Map<String, Double> pivot(
+            Map<String, List<Double>> groupedData) {
 
-        Map<String, Map<String, Double>> table = new HashMap<>();
-
-        for (int i = 0; i < rows.size(); i++) {
-            table
-                .computeIfAbsent(rows.get(i), r -> new HashMap<>())
-                .merge(columns.get(i), values.get(i), Double::sum);
-        }
-        return table;
+        Map<String, Double> pivot = new HashMap<>();
+        groupedData.forEach(
+            (k, v) -> pivot.put(k, AggregationFunctions.sum(v))
+        );
+        return pivot;
     }
 }
