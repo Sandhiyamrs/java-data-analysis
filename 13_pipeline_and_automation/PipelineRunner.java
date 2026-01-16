abcb2f487b585f4973e0a43c7b7aa95c3a4b07d9
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-16 22:56:04 +0530

    Update PipelineRunner.java

--- 13_pipeline_and_automation/PipelineRunner.java
+++ 13_pipeline_and_automation/PipelineRunner.java
@@ -2,6 +2,8 @@ public class PipelineRunner {
 
     public static void main(String[] args) {
         PipelineContext context = new PipelineContext();
-        PipelineLogger.log("Pipeline started");
+        DataPipeline pipeline = new DataPipeline();
+
+        pipeline.run(context);
     }
 }
